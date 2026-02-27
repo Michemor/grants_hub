ab37c6f56969006e60c8400b9ad1d98d16e21320
Michemor <yatoijema@gmail.com>
2026-02-27 22:40:03 +0300

    Modified allowed origins list

--- app/main.py
+++ app/main.py
@@ -98,11 +98,17 @@ app = FastAPI(
     version="1.0.0",
 )
 
+allowed_origins_str = os.getenv("ALLOWED_ORIGINS", "")
+
+origins_list = [
+    origin.strip() 
+    for origin in allowed_origins_str.split(",") 
+    if origin.strip()
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "*").split(
-        ","
-    ),  # Adjust to fit frontend domain in production
+    allow_origins=origins_list,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
     allow_credentials=True,
